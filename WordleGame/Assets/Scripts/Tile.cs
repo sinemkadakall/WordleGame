7288ce2cf22fbeaa5a8a5e1f758d2c52cc755a07
sinemsooplay <sinemkadakall@gmail.com>
2024-09-04 16:51:54 +0300

    Entering  Letter

---
+++ WordleGame/Assets/Scripts/Tile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    public char letter {  get; private set; }
+
+    private void Awake()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void setLetter(char letter)
+    {
+        this.letter = letter;
+        text.text = letter.ToString();
+
+
+    }
+
+}
